51e07e9c6970e57f5d4838abc0727cfb38c847e1
denis4ik23 <denis4ik2387@gmail.com>
2022-09-07 00:30:35 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String massage;
+
+    public void printInfo() {
+        System.out.println("есть ошибка: " + active);
+        System.out.println("количество: " + status);
+        System.out.println("название: " + massage);
+    }
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String massage) {
+        this.active = active;
+        this.status = status;
+        this.massage = massage;
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error error1 = new Error(true, 1, "ошибка 404");
+        error1.printInfo();
+        Error error2 = new Error(true, 2 , "ошибка 401");
+        error2.printInfo();
+    }
+}
