1e46ef2a3e67ba09bb97b242159a6886e10e82ad
denis4ik23 <denis4ik23@gmail.com>
2026-01-24 17:33:19 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("denis4ik2387@gmail.com", "Cheklov Denis Gennadievich");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
